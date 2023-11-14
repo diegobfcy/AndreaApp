49f0ea06013ee49d57629ab49c31e58aa65391c3
unknown <diegobfcy@gmail.com>
2023-11-13 21:31:51 -0500

    Añadida la ruta para pasar a la bienvenida de andrea

--- src/components/HomeLayout/HomeLayout.js
+++ src/components/HomeLayout/HomeLayout.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './HomeLayout.css'; // Asegúrate de tener un archivo CSS para estilos
 import backgroundImg from '../../assets/icons/AndreaFondoImg.png'; // Ajusta la ruta de tu imagen
 import AndreaLink from '../../assets/icons/AndreaLink.png';
 
 
 const HomeLayout = () => {
+
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    // Navega a la página "bienvenida-page" al hacer clic en el botón
+    navigate('/bienvenida-page');
+  };
+
   return (
     <div className="HomeLayout-MainContainer">
       <div className="HomeLayout-Background">
@@ -16,10 +25,9 @@ const HomeLayout = () => {
           alt="Imagen Centrada"
           className="HomeLayout-CenteredImage"
         />
-        <button className="HomeLayout-Boton">Empieza ahora</button>
+        <button className="HomeLayout-Boton" onClick={handleButtonClick}>Empieza ahora</button>
       </div>
 
-      {/* Contenido de tu página */}
     </div>
   );
 };
